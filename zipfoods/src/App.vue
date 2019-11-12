8d776249be516bc1cded9ccdf4d482f9eaf70ec6
Andrea S <andreasalkey@gmail.com>
2019-11-11 19:46:29 -0500

    initial commit

---
+++ zipfoods/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <img id="logo" alt="ZipFoods logo" src="./assets/images/zipfoods-logo.png">
+    <nav>
+        <ul>
+            <li v-for='link in links' :key='link'>
+                <router-link :to='paths[link]' exact>{{ link }}</router-link>
+            </li>
+        </ul>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import {products} from './products.js';
+
+export default {
+  name: 'app',
+  data: function (){
+        return {
+            /* Default page to start with */
+            page: 'home',
+            /* Store links in an array to maintain order */
+            links: ['home', 'products', 'categories'],
+            paths: {
+                home: '/',
+                products: '/products',
+                categories: '/categories'
+            },
+            products:products
+        }
+    }
+}
+</script>
+
+<style lang='scss'>
+@import './assets/css/zipfoods.scss';
+</style>
