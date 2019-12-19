611e6d762884fa3dc9044e00eb53b4965a938874
Andrea S <andreasalkey@gmail.com>
2019-12-19 18:26:57 -0500

    [+] warning notifications

--- p4/src/components/pages/PostPage.vue
+++ p4/src/components/pages/PostPage.vue
@@ -4,39 +4,47 @@
         <span class='post-date'>{{post.post_date}}</span>
         <div class='post-content'>{{post.post_content}}</div>
         <hr>
+        <transition name='fade'>
+            <div class='alert alert-success' v-if='favesAdded'>You faved this!</div>
+        </transition>
         <button class='btn btn-primary' @click='fave(post.id)'> add to faves </button>
         <hr>
         <div class='ratings' v-if='!post.post_rating'>
-            <h5>Rate this post: </h5>
-            <div class="alert alert-danger" v-if='formHasErrors'>Correct the errors before submitting</div>
-            <form @submit.prevent="handleSubmit(post)">
-                <div class="form-group">
-                    <label> Name: </label>
-                    <input type="text" :class='{ "form-input-error": $v.rating.author.$error }' class="form-control" placeholder="Enter your name" v-model="$v.rating.author.$model" />
-                <div v-if='$v.rating.author.$error'>
-                        <div class='alert alert-warning' v-if='!$v.rating.author.required'>Your name is required.</div>
+            <transition name='fade'>
+                <div class='alert alert-success' v-if='ratingAdded'>Rating added!</div>
+            </transition>
+            <div id="ratings-form">
+                <h5>Rate this post: </h5>
+                <div class="alert alert-danger" v-if='formHasErrors'>Correct any errors before submitting</div>
+                <form @submit.prevent="handleSubmit(post)">
+                    <div class="form-group">
+                        <label> Name: </label>
+                        <input type="text" :class='{ "form-input-error": $v.rating.author.$error }' class="form-control" placeholder="Enter your name" v-model="$v.rating.author.$model" />
+                    <div v-if='$v.rating.author.$error'>
+                            <div class='alert alert-warning' v-if='!$v.rating.author.required'>Your name is required.</div>
+                        </div>
                     </div>
-                </div>
-                <div class="form-group">
-                    <label>Feedback: </label>
-                    <textarea class="form-control"  v-model="$v.rating.comment.$model"></textarea>
-                    <div v-if='$v.rating.comment.$error'>
-                        <div class='alert alert-warning' v-if='!$v.rating.comment.minLength'>Feedback needs to be atleast 30 characters</div>
-                        <div class='alert alert-warning' v-if='!$v.rating.comment.required'>Please enter feedback</div>
+                    <div class="form-group">
+                        <label>Feedback: </label>
+                        <textarea class="form-control"  v-model="$v.rating.comment.$model"></textarea>
+                        <div v-if='$v.rating.comment.$error'>
+                            <div class='alert alert-warning' v-if='!$v.rating.comment.minLength'>Feedback needs to be atleast 30 characters</div>
+                            <div class='alert alert-warning' v-if='!$v.rating.comment.required'>Please enter feedback</div>
+                        </div>
                     </div>
-                </div>
-                <div class="form-group">
-                    <label>Score:</label>
-                    <input type="number" min="0" max="10" class="form-control"  v-model="$v.rating.score.$model"/>
-                    <div v-if='$v.rating.score.$error'>
-                        <div class='alert alert-warning' v-if='!$v.rating.score.required'>Enter a score</div>
-                        <div class='alert alert-warning' v-if='!$v.rating.score.between'>Enter a score between 0-10</div>
+                    <div class="form-group">
+                        <label>Score:</label>
+                        <input type="number" min="0" max="10" class="form-control"  v-model="$v.rating.score.$model"/>
+                        <div v-if='$v.rating.score.$error'>
+                            <div class='alert alert-warning' v-if='!$v.rating.score.required'>Enter a score</div>
+                            <div class='alert alert-warning' v-if='!$v.rating.score.between'>Enter a score between 0-10</div>
+                        </div>
                     </div>
-                </div>
-                <div class="form-group">
-                    <input type="submit" value="Submit review" class="btn btn-info"/>
-                </div>
-            </form>
+                    <div class="form-group">
+                        <input type="submit" value="Submit review" class="btn btn-info"/>
+                    </div>
+                </form>
+            </div>
         </div>
         <router-link :to='{ name:"posts"}'> Back to all posts</router-link>
     </div>
@@ -56,7 +64,10 @@ export default {
                 score: '',
                 comment:'',
             },
-            formHasErrors: false
+            formHasErrors: false,
+            ratingAdded: false,
+            favesAdded: false,
+            hideForm:false,
         }
             
     },
@@ -89,6 +100,10 @@ export default {
         fave: function (id) {
             let faves = new app.Faves();
             faves.create(id);
+            
+            this.favesAdded = true;
+                
+            setTimeout(() => (this.favesAdded = false), 2000)
         },
         handleSubmit: function(post){
             this.$v.$touch();
@@ -103,6 +118,11 @@ export default {
                 });
                 
                 this.formHasErrors = false;
+                this.ratingAdded = true;
+                
+                document.getElementById("ratings-form").innerHTML = ""; 
+                
+                setTimeout(() => (this.ratingAdded = false), 2000)
             }else{
                 this.formHasErrors = this.$v.$anyError;
             }
